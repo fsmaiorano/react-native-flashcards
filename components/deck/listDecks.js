f4799d28157e5b0c8f00294b55869419b2a85042
Fábio  Maiorano <fsmaiorano@MacBook-Air-de-Fabio.local>
2018-02-14 21:36:59 -0200

    add: deckItem style

--- components/deck/listDecks.js
+++ components/deck/listDecks.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { View, Text, ScrollView, TouchableOpacity, Button, StyleSheet } from 'react-native'
-import { blue } from '../../template/colors';
+import { Platform, View, Text, ScrollView, TouchableOpacity, Button, StyleSheet } from 'react-native'
+import { blue, white } from '../../template/colors';
 import { getDecks, reset } from './actions';
 
 class ListDecks extends Component {
@@ -26,11 +26,12 @@ class ListDecks extends Component {
                             let deck = decks[selectedDeck];
                             return (
                                 <TouchableOpacity
+                                    style={styles.deckItem}
                                     key={deck.title}
                                     onPress={() => navigation.navigate('DetailDeck', { deck })}
                                 >
-                                    <Text style={[{ fontSize: 18 }]}>{deck.title}</Text>
-                                    <Text style={[{ color: blue }]}>{deck.questions.length} cards</Text>
+                                    <Text style={[{ textAlign: 'center' }, { fontSize: 18 }]}>{deck.title}</Text>
+                                    <Text style={[{ textAlign: 'center' }, { color: blue }]}>{deck.questions.length} cards</Text>
                                 </TouchableOpacity>
                             );
                         })
@@ -42,7 +43,7 @@ class ListDecks extends Component {
         else {
             return (
                 <View style={styles.noDecks}>
-                    <Text style={{fontSize: 30}}>No Decks :(</Text>
+                    <Text style={{ fontSize: 30 }}>No Decks :(</Text>
                 </View>
             )
         }
@@ -55,7 +56,23 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         flex: 1,
         flexDirection: 'column',
-    }
+    },
+    deckItem: {
+        backgroundColor: white,
+        borderRadius: Platform.OS == "ios" ? 16 : 2,
+        padding: 20,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 17,
+        justifyContent: 'center',
+        shadowRadius: 3,
+        shadowOpacity: 0.8,
+        shadowColor: 'rgba(0,0,0,0.24)',
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        }
+    },
 })
 
 const mapStateToProps = (state) => {
