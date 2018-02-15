a3f7dbbeb4ebd4efe06e1a4a4ee6a1faab89e2fd
Fábio  Maiorano <fsmaiorano@MacBook-Air-de-Fabio.local>
2018-02-14 22:11:33 -0200

    add: style in newDeck component

--- components/deck/newDeck.js
+++ components/deck/newDeck.js
@@ -2,24 +2,37 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { View, Text, TextInput, Platform, Button, StyleSheet } from 'react-native'
 import { newDeck } from './actions';
+import {white} from '../../template/colors';
 
 class NewDeck extends Component {
     state = {
-        title: ''
+        title: '',
+        isInvalid: false
     }
 
     createDeck = () => {
-        const { title } = this.state;
-        this.props.dispatch(newDeck(title, { title, questions: [] }));
-        this.props.navigation.goBack();
+        const { title, isInvalid } = this.state;
+        if(title !== ''){
+            this.props.dispatch(newDeck(title, { title, questions: [] }));
+            this.setState({isInvalid: false});
+            this.props.navigation.goBack();
+        }
+        else {
+            this.setState({isInvalid: true});
+        }
     }
 
     render() {
-        const { title } = this.state;
+        const { title, isInvalid } = this.state;
         return (
-            <View>
-                <Text>What is the title of your new deck ?</Text>
-                <TextInput onChangeText={(title) => this.setState({ title })} placeholder={'Insert here the name your new deck'}></TextInput>
+            <View style={styles.container}>
+                <Text style={{ fontSize: 35, textAlign: 'center' }}>
+                    What is the title of your new deck?
+                </Text>
+                <TextInput style={[styles.input, { margin: 40, marginBottom: 5, borderColor: isInvalid ? 'red' : 'gray', borderWidth: 1 }]}
+                    onChangeText={(title) => this.setState({ title })}
+                    placeholder={'Insert here the name your new deck'}>
+                </TextInput>
                 <Button onPress={this.createDeck} title='Create Deck'></Button>
             </View>
         )
@@ -27,7 +40,17 @@ class NewDeck extends Component {
 }
 
 const styles = StyleSheet.create({
-    
+    container: {
+        flex: 1,
+        padding: 20,
+        paddingTop: 40,
+        backgroundColor: white
+    },
+    input: {
+        margin: 15,
+        padding: 5,
+        borderRadius: 4,
+    },
 })
 
 const mapStateToProps = (state) => {
